515bf415d897c7834c6a5cd82bb6ab03925ebacc
Nedoluzhko Denis <denisnedoluzhko1902@gmail.com>
2021-11-10 22:51:50 +0300

    tree:task completed

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -3,10 +3,61 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
+	"strconv"
 )
+
+func dirTreeRec(out io.Writer, path string, printFiles bool, dirPrefix string) (ferr error) {
+	files, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		ferr = err
+		return
+	} else {
+		i_lastprinted := -1
+		for i, file := range files {
+			if file.IsDir() || printFiles {
+				i_lastprinted = i
+			}
+		}
+
+		dirChildPrefix := dirPrefix + "├───"
+		childDirPrefix := dirPrefix + "│\t"
+
+		for i, file := range files {
+			if i == i_lastprinted {
+				dirChildPrefix = dirPrefix + "└───"
+				childDirPrefix = dirPrefix + "\t"
+			}
+
+			if file.IsDir() {
+				fmt.Fprintf(out, "%s%s\n", dirChildPrefix, file.Name())
+
+				dirTreeRec(out, filepath.Join(path, file.Name()), printFiles, childDirPrefix)
+			} else {
+				if printFiles {
+					info, _ := os.Stat(filepath.Join(path, file.Name()))
+					filesize := strconv.FormatInt(info.Size(), 10)
+					if "0" == filesize {
+						filesize = "empty"
+					} else {
+						filesize += "b"
+					}
+					fmt.Fprintf(out, "%s%s (%s)\n", dirChildPrefix, file.Name(), filesize)
+				}
+			}
+		}
+	}
+
+	return
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) (ferr error) {
+	ferr = dirTreeRec(out, path, printFiles, "")
+	return
+}
 
 func main() {
 	out := os.Stdout
